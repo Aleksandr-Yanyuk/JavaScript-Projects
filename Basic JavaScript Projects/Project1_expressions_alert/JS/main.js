142215c09e3a7413e8cbb9d668efef23169596b8
Alex745i <alex745i@gmail.com>
2022-12-07 15:35:35 -0800

    BASIC JAVASCRIPT PROJECTS SUBMISSION ASSIGNMENT

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,12 @@
+window.alert("Hello, world!");  //This pops up a window alert
+
+var Sent1 = ("This is the beginning of the string"), Sent2 = (" and this is the end of the string "); /////this is the variables
+
+document.write(Sent1+Sent2)   //writing an expression
+
+document.write(3 + 3);     //writing an expression
+
+var Z = ("Lisa told bart, \"knock it off, Bart! Or i\ll tell dad!!\"<br>\"eat my shorts!\" Bart responded.") + //Concatenated 
+("\"Be who you are and say what you feel, because those who don\'t mind don\'t matter and those who matter don\'t mind\"")//Concatenated 
+
+document.write(Z)
---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/notes.js
@@ -0,0 +1,28 @@
+alert("Hello, world!");
+
+window.alert("Hello, world!");
+
+document.write('hello,world!');
+
+var Z = "This is a string";
+
+document.write(Z)
+
+var B = "Concatenated" + "STRING"
+
+document.write("\"i\'ll do this i\'ll do that\" and do that i\'ll");
+
+var Z = ("Lisa told bart, \"knock it off, Bart! Or i\ll tell dad!!\"<br>\"eat my shorts!\" Bart responded.") + 
+("\"Be who you are and say what you feel, because those who don\'t mind don\'t matter and those who matter don\'t mind\"")
+document.write(Z)
+
+var family = "the arezzinis", dad = "jeremiah", mom = "hermoine"
+document.write(mom);
+
+var family="yanyuk", dad="fedor", mom="nelya", brother="roman", sister="inna";
+document.write(family)
+
+a=100,b=200,c=300,d=400,e=500;
+
+int, a=10, b=20, c=30, d=40, e=50;
+System.out.println("Sum of a and b is: "+a+b);
