042a420e6de2b5d8759610942755e70805ecc843
Alex745i <alex745i@gmail.com>
2022-12-08 15:08:32 -0800

    PROJECT5_COMPARISONS_TYPE_COERCION SUBMISSION ASSIGNMENT

--- Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -3,12 +3,6 @@ function my_Function() {
     document.getElementById("Test").innerHTML = isNaN('007');
 }
 
-document.write(2E310);
-document.write(-2E310);
-
-// document.write(10 > 2);     True
-// document.write(10 < 2);     false
-
 console.log(2 + 2);
 
 document.write(10 == 10); // True
@@ -18,10 +12,16 @@ document.write(5 > 10 && 10 > 4); // false
 document.write(5 > 10 || 10 > 4); // true
 document.write(5 > 10 || 10 > 20); // false
 
+
 //Words and numbers // 
 document.write(typeof "Word");
 document.write(typeof  3);
 document.write("10" + 5);
+X = 1000;
+Y = 1000;
+document.write(X === Y); ///true
+document.write(2E310);   //Infinity
+document.write(-2E310);  //infinity
 
 function not_Function() {
     document.getElementById("Not").innerHTML = !(20 > 10);
@@ -31,9 +31,9 @@ function not1_Function() {
     document.getElementById("Not1").innerHTML = !(5 > 10);
 }
 
-function Ride_Function() {
-    var Height, Can_ride;
-    Height = document.getElementById("Height").value;
-    Can_Ride = (Height < 52) ? "You are too short":"You are tall enough";
-    document.getElementById("Ride").innerHTML = Can_ride + "to ride.";
+function age_Function() {
+    var dob, old_enough;
+    dob = document.getElementById("dob_age1").value;
+    old_enough = (dob < 18) ? "under age":"of age";
+    document.getElementById("dob_age2").innerHTML = old_enough + " to Enter";
 }
